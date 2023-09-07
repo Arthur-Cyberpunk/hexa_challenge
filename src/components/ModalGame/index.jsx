4ad76515c96fd48b195b7633db296ab990546055
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-09-07 01:28:18 -0300

    create modal time and score

--- src/components/ModalGame/index.jsx
+++ src/components/ModalGame/index.jsx
@@ -6,7 +6,9 @@ const ModalGame = () => {
       <div className="boxGuessColor">
         <h1 className="title">Guess the color</h1>
         <div className="boxTime">
-          <p className="time">Remaning <br /> Time (s) <br /> 30</p>
+          <p className="time">
+            Remaning <br /> Time (s) <br /> 30
+          </p>
           <p className="time">Restart</p>
           <div className="boxScore">
             <p className="score">High Score 20</p>
@@ -14,7 +16,7 @@ const ModalGame = () => {
           </div>
         </div>
         <div className="boxColor">
-          <button>Start</button>
+          <button className="start">Start</button>
         </div>
       </div>
     </div>
