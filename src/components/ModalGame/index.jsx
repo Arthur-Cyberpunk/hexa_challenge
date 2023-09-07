c198e8258d0632830982aec3c6352373e381fe28
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-09-07 20:58:39 -0300

    create random colors - show the colors - restart button

--- src/components/ModalGame/index.jsx
+++ src/components/ModalGame/index.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import "./styles.scss";
 
 const ModalGame = () => {
+  const [color, setColor] = useState([]);
+  const [appearColor, setAppear] = useState(false);
+
+  const randomColors = () => {
+    for (let i = 0; i < 3; i++) {
+      const hex = ((Math.random() * 0xffffff) << 0).toString(16);
+      setColor((prevArray) => [...prevArray, `#${hex}`]);
+    }
+    setAppear(true)
+  };
+
+  const handleClick = (value) => {
+    alert(`Você clicou em: ${value}`);
+  };
+
+  const restart = () => {
+    setColor([''])
+    setAppear(false)
+  }
+
   return (
     <div className="container">
       <div className="boxGuessColor">
@@ -9,15 +30,26 @@ const ModalGame = () => {
           <p className="time">
             Remaning <br /> Time (s) <br /> 30
           </p>
-          <p className="time">Restart</p>
+          <span className="restartGame" onClick={restart}>Restart</span>
           <div className="boxScore">
             <p className="score">High Score 20</p>
             <p className="score">Score -</p>
           </div>
         </div>
         <div className="boxColor">
-          <button className="start">Start</button>
+          <button className="start" onClick={randomColors}>
+            Start
+          </button>
         </div>
+        {appearColor ? (
+          <div className="boxRandomColors">
+            {color.map((hexaColor, index) => (
+              <li key={index} onClick={() => handleClick(hexaColor)}>{hexaColor}</li>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
