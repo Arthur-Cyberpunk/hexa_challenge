1ac15ce1652ce29b1960eee944eefac0374ca4ad
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-09-08 16:04:57 -0300

    adjust all logic program

--- src/components/ModalGame/index.jsx
+++ src/components/ModalGame/index.jsx
@@ -2,58 +2,72 @@ import { useEffect, useState } from "react";
 import "./styles.scss";
 
 const ModalGame = () => {
-  const [color, setColor] = useState([]);
-  const [appearColor, setAppear] = useState(false);
-  const [seconds, setSeconds] = useState(5);
-  const [isActive, setIsActive] = useState(false);
-  const [selectedColor, setSelectedColor] = useState('');
+  const [currentColor, setCurrentColor] = useState('');
+  const [options, setOptions] = useState([]);
+  const [score, setScore] = useState(0);
+  const [secondsLeft, setSecondsLeft] = useState(2);
+  const [active, setActive] = useState(false);
 
-  const randomColors = () => {
-    for (let i = 0; i < 3; i++) {
-      const hex = ((Math.random() * 0xffffff) << 0).toString(16);
-      setColor((prevArray) => [...prevArray, `#${hex}`]);
+  const generateRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-    setAppear(true);
-    setIsActive(true);
-  };
+    return color;
+  }
 
-  const handleClick = (value) => {
-    alert(`Você clicou em: ${value}`);
-  };
+  const startGame = () => {
+    if (secondsLeft === 0) {
+        setSecondsLeft(2)
+    }
+    
+    const correctColor = generateRandomColor();
+    const randomColor1 = generateRandomColor();
+    const randomColor2 = generateRandomColor();
 
-  const restart = () => {
-    setColor([]);
-    setAppear(false);
-    setIsActive(false);
-    setSeconds(5)
-    setSelectedColor('')
-  };
+    const randomOptions = [correctColor, randomColor1, randomColor2];
+    randomOptions.sort(() => Math.random() - 0.5);
+
+    setCurrentColor(correctColor);
+    setOptions(randomOptions);
+    setActive(true)
+  }
+
+  const checkAnswer = (selectedColor) => {
+    console.log(currentColor)
+    console.log(selectedColor)
+    
+    if (selectedColor === currentColor) {
+      setScore(score + 5);
+    } else if (selectedColor !== currentColor) {
+        setScore(score - 1);
+    }
+    startGame();
+  }
 
   useEffect(() => {
-    let intervalId;
-
-    if (isActive) {
-      intervalId = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds - 1);
+    let seconds;
+    if (secondsLeft > 0 && active) {
+        seconds = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
       }, 1000);
-
-      console.log('alo')
-
-      const randomIndex = Math.floor(Math.random() * color.length);
-      setSelectedColor(color[randomIndex]);
-    } else {
-      clearInterval(intervalId);
-      restart();
+    } else if (secondsLeft === 0) {
+      setCurrentColor('');
+      setActive(false)
     }
 
-    return () => clearInterval(intervalId);
-  }, [isActive]);
+    return () => clearTimeout(seconds);
+  }, [secondsLeft, active]);
 
-  useEffect(() => {
-    if (seconds === 0) {
-        setIsActive(false);
-    }
-  }, [seconds])
+  const restartGame = () => {
+    setCurrentColor('');
+    setOptions([]);
+    setScore(0);
+    setSecondsLeft(2);
+    startGame();
+    setActive(true)
+  }
 
   return (
     <div className="container">
@@ -61,31 +75,31 @@ const ModalGame = () => {
         <h1 className="title">Guess the color</h1>
         <div className="boxTime">
           <p className="time">
-            Remaning <br /> Time (s) <br /> {seconds}
+            Remaning <br /> Time (s) <br /> {secondsLeft}
           </p>
           <span
-            className={`restartGame ${isActive ? "active" : ""}`}
-            onClick={restart}
+            className={`restartGame ${active ? "active" : ""}`}
+            onClick={restartGame}
           >
             Restart
           </span>
           <div className="boxScore">
             <p className="score">High Score 20</p>
-            <p className="score">Score -</p>
+            <p className="score">{`Score ${score ? score : "-"}`}</p>
           </div>
         </div>
-        <div className="boxColor" style={{backgroundColor: selectedColor}}>
+        <div className="boxColor" style={{backgroundColor: currentColor}}>
           <button
-            className={`start ${isActive ? "active" : ""}`}
-            onClick={randomColors}
+            className={`start ${active ? "active" : ""}`}
+            onClick={startGame}
           >
             Start
           </button>
         </div>
-        {appearColor ? (
+        {active ? (
           <div className="boxRandomColors">
-            {color.map((hexaColor, index) => (
-              <li key={index} onClick={() => handleClick(hexaColor)}>
+            {options.map((hexaColor, index) => (
+              <li key={index} onClick={() => checkAnswer(hexaColor)}>
                 {hexaColor}
               </li>
             ))}
