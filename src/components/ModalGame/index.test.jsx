2d302305fb2ce8afdc85b881da089fca311f44f6
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-09-11 22:50:05 -0300

    create tests

--- src/components/ModalGame/index.test.jsx
+++ src/components/ModalGame/index.test.jsx
@@ -1,10 +1,24 @@
+import '@testing-library/react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import ModalGame from '.';
+import GameProvider from '../../contexts/gameContext';
+
+const setActive = jest.fn();
+const startGame = jest.fn();
+const restart = jest.fn();
+
+const renderComponent = ()=> {
+    return render(
+        <GameProvider value={{setActive, startGame, restart}}>
+            <ModalGame />
+        </GameProvider>
+    )
+}
 
 describe('ModalGame', () => {
   it('should render the texts', () => {
-    render(<ModalGame />);
+    render(<ModalGame />)
     
     expect(screen.getByText('Guess the color')).toBeInTheDocument();
     expect(screen.getByText('Remaning Time (s)')).toBeInTheDocument();
@@ -15,17 +29,23 @@ describe('ModalGame', () => {
   });
 
   it('should render the button with the text "Start"', () => {
-  render(<ModalGame text='Start' />);
+    render(<ModalGame />)
 
-  const startButton = screen.getByRole('button', { name: /start/i});
-  expect(startButton).toBeInTheDocument()
+    const startButton = screen.getByRole('button', { name: /start/i});
+    expect(startButton).toBeInTheDocument()
   });
 
   it('should call function on button click', () => {
-    const fn = jest.fn()
-    render(<ModalGame text='Start' onClick={fn} />);
+
+    renderComponent()
   
-    const startButton = screen.getByRole('button', { name: /start/i});
-    fireEvent.click(startButton)
+    const startButton = screen.getByText('Start');
+    expect(startButton).toBeInTheDocument();
+  
+    fireEvent.click(startButton);
+
+    console.log(startGame)
+  
+    expect(startGame).toHaveBeenCalledTimes(1)
     });
 })
